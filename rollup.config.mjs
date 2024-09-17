41f0063bbba32809f4b9e2b2f12291b9fcca63db
Tom Wilson <twilson90@hotmail.com>
2024-09-17 11:43:28 +0100

    blah

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,36 +1,37 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json' with { type: "json" };
 
 export default [
   {
-    input: './src/index.js',
+    input: pkg.source,
     output: [
         {
-          file: pkg.exports['.'].import,
+          file: pkg.module,
           format: 'esm',
           sourcemap: true,
         },
         {
-          file: pkg.exports['.'].require,
+          file: pkg.main,
           format: 'cjs',
           sourcemap: true,
         }
     ],
     plugins: [
-        babel({
-          presets: ["@babel/preset-env"],
-          /* plugins: [
-            ["polyfill-corejs3", { "method": "usage-global" }],
-            "@babel/transform-runtime"
-          ], */
-          exclude: /node_modules/,
-          skipPreflightCheck: true,
-          babelHelpers: 'runtime'
-        }),
-        resolve(),
-        commonjs(),
+      babel({
+        presets: ["@babel/preset-env"],
+        /* plugins: [
+          ["polyfill-corejs3", { "method": "usage-global" }],
+          "@babel/transform-runtime"
+        ], */
+        exclude: /node_modules/,
+        skipPreflightCheck: true,
+        babelHelpers: 'runtime'
+      }),
+      resolve(),
+      commonjs(),
     ],
   }
 ];
